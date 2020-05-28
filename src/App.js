8c7db8a13e72c67025122d35e672d681a925ece0
nikmargit <nikola.margit@gmail.com>
2020-05-28 21:07:28 +0200

    add initial data and implement basic game logic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useTimeout from './useTimeout';
 
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
 function App() {
+    const [letters, setLetters] = useState([]);
+    const [randomLetter, setRandomLetter] = useState(null);
+
+    useEffect(() => {
+        const initialLetters = ALPHABET.map((character, index) => ({
+            character,
+            ordinal: index + 1,
+            status: null,
+        }));
+        setLetters(initialLetters);
+    }, [setLetters]);
+
     const [startTimeout, stopTimeout] = useTimeout(() =>
         console.log('timeout')
     );
 
-    function handleAnswer() {
-        stopTimeout();
-        console.log('correct or wrong');
+    const startNewGame = (lett) => {
+        const unnansweredLetters = lett.filter((letter) => !letter.status);
+        const random =
+            unnansweredLetters[
+                Math.floor(Math.random() * unnansweredLetters.length)
+            ];
+        setRandomLetter(random);
         startTimeout(3000);
+    };
+
+    function handleAnswer({ target }) {
+        stopTimeout();
+        const correctAnswer =
+            target.value.toLowerCase() === randomLetter.character;
+        const lettersCopy = [...letters].map((letter) => {
+            if (letter.character === randomLetter.character) {
+                return {
+                    ...letter,
+                    status: correctAnswer ? 'correct' : 'wrong',
+                };
+            }
+            return letter;
+        });
+        setLetters(lettersCopy);
+        startNewGame(lettersCopy);
+        target.select();
     }
 
     return (
         <>
-            <h1>ALPHABET GAME</h1>
-            <button onClick={() => startTimeout(3000)}>start game</button>
-            <button onClick={handleAnswer}>answer</button>
+            {randomLetter && <h1>{randomLetter.ordinal}</h1>}
+            <label htmlFor="letter">Input letter</label>
+            <input
+                type="text"
+                name="letter"
+                onChange={handleAnswer}
+                maxLength={1}
+            />
+            <button onClick={() => startNewGame(letters)}>start game</button>
             <button onClick={stopTimeout}>stopGame</button>
         </>
     );
