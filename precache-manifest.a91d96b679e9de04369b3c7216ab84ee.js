584d0a0b541e45c64697dcba12394a20ab31f7fc
nikmargit <nikola.margit@gmail.com>
2020-05-29 13:40:26 +0200

    Updates

---
+++ precache-manifest.a91d96b679e9de04369b3c7216ab84ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9397c115be8d8e28f67b02ea719a4bda",
+    "url": "/alphabet-reflex-game/index.html"
+  },
+  {
+    "revision": "b326a72cfd786eb6866e",
+    "url": "/alphabet-reflex-game/static/css/main.706d982b.chunk.css"
+  },
+  {
+    "revision": "dacadda31246c905167a",
+    "url": "/alphabet-reflex-game/static/js/2.c8a2b7d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alphabet-reflex-game/static/js/2.c8a2b7d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b326a72cfd786eb6866e",
+    "url": "/alphabet-reflex-game/static/js/main.71fb067a.chunk.js"
+  },
+  {
+    "revision": "d1cdc32759c9afa25029",
+    "url": "/alphabet-reflex-game/static/js/runtime-main.cf0874e3.js"
+  }
+]);
